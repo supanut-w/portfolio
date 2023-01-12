02f2754acaa050c3d236dcac558b1ef68917f184
supanut-wj <supanut.wj@gmail.com>
2023-01-12 14:48:29 +0700

    complete nav bar structure. pending on updating logo pictures

--- client/src/Components/Sidebar/SidebarList.js
+++ client/src/Components/Sidebar/SidebarList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SidebarList.css";
 import profilepic from "../../Image/laptop.jpg";
+import { FcHome } from "react-icons/fc";
 
 const SidebarList = ({ expandSidebar }) => {
   return (
@@ -11,10 +12,62 @@ const SidebarList = ({ expandSidebar }) => {
             <img src={profilepic} alt="profile picture" />
           </div>
 
-          
+          <ul>
+            <li className="nav-item">
+              <FcHome size={25} /> Home
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} /> About
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} /> Work Experience
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} /> Tech Stack
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} /> Education
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} /> Projects
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} /> Testimonial
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} /> Contact
+            </li>
+          </ul>
         </div>
       ) : (
-        <div></div>
+        <div className="navbar-items-only-icons">
+          <ul>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+          </ul>
+        </div>
       )}
     </React.Fragment>
   );
